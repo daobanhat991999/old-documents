4a581bbddf4dc0c819c94bf8efad12b521f2c621
Nguyen Anh Tuan <daobanhat991999@gmail.com>
2019-11-18 17:39:53 +0700

    cap nhap data

---
+++ Desktop/New folder (2)/listsanpham.js
@@ -0,0 +1,173 @@
+let listSanPham = [
+    // {
+    //     "img": "./images/product_images/americano.jpg",
+    //     "name": "Americano",
+    //     "price": "55000",
+    //     "type": ["món đặc biệt","espresso"],
+    //     "countTotal":0,
+    //     "issale":0
+    // },
+    // {
+    //     "img": "./images/product_images/caramel_macchiato.jpg",
+    //     "name": "caramel macchiato",
+    //     "price": "65000",
+    //     "type": ["món đặc biệt","espresso"],
+    //     "countTotal":0,
+    //     "issale":0
+    // },
+    // {
+    //     "img": "./images/product_images/espresso.jpg",
+    //     "name": "espresso",
+    //     "price": "55000",
+    //     "type": ["món đặc biệt","espresso"],
+    //     "countTotal":0,
+    //     "issale":0
+    // },
+    // {
+    //     "img": "./images/product_images/milk_coffee.jpg",
+    //     "name": "nâu đá",
+    //     "price": "30000",
+    //     "type": ["món đặc biệt","cà phê phin"],
+    //     "countTotal":0,
+    //     "issale":0
+    // },
+    // {
+    //     "img": "./images/product_images/tra_phuc_bon_tu.png",
+    //     "name": "trà phúc bồn tử",
+    //     "price": "65000",
+    //     "type": ["món đặc biệt","trà"],
+    //     "countTotal":0,
+    //     "issale":0
+    // },
+    // {
+    //     "img": "./images/product_images/tra_xoai_macchiato.jpg",
+    //     "name": "trà xoài macchiato",
+    //     "price": "65000",
+    //     "type": ["món đặc biệt","trà"],
+    //     "countTotal":0,
+    //     "issale":0
+    // },
+    // {
+    //     "img": "./images/product_images/americano.jpg",
+    //     "name": "Americano",
+    //     "price": "55000",
+    //     "type": "món đặc biệt",
+    //     "countTotal":0,
+    //     "issale":0
+    // },
+    {
+        "img": "./images/product_images/caramel_macchiato.jpg",
+        "name": "caramel macchiato",
+        "price": "65000",
+        "type": "espresso",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/product_images/espresso.jpg",
+        "name": "espresso",
+        "price": "55000",
+        "type": "espresso",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/coffee_bean/coffee_bean_metal.JPG",
+        "name": "metal coffee bean",
+        "price": "300000",
+        "type": "coffeeBean",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/coffee_bean/coffee_bean_wood.JPG",
+        "name": "wood coffee bean",
+        "price": "300000",
+        "type": "coffeeBean",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/coffee_bean/coffee_bean_water.JPG",
+        "name": "water coffee bean",
+        "price": "300000",
+        "type": "coffeeBean",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/coffee_bean/coffee_bean_fire.JPG",
+        "name": "fire coffee bean",
+        "price": "300000",
+        "type": "coffeeBean",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/coffee_bean/coffee_bean_earth.JPG",
+        "name": "earth coffee bean",
+        "price": "300000",
+        "type": "coffeebean",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/coffee_bean/coffee_bean_for_test.JPG",
+        "name": "coffee bean for test",
+        "price": "20000",
+        "type": "coffeebean",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/product_images/den.jpg",
+        "name": "cà phê đen",
+        "price": "29000",
+        "type": "cà phê phin",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/product_images/milk_coffee.jpg",
+        "name": "cà phê nâu",
+        "price": "32000",
+        "type": "eà phê phin",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/product_images/bac_siu.jpg",
+        "name": "bạc sỉu",
+        "price": "32000",
+        "type": "cà phê phin",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/product_images/tra_phuc_bon_tu.png",
+        "name": "trà phúc bồn tử",
+        "price": "65000",
+        "type": "trà",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/product_images/tra_xoai_macchiato.jpg",
+        "name": "trà xoài macchiato",
+        "price": "65000",
+        "type": "trà",
+        "countTotal":0,
+        "issale":0
+    },
+    {
+        "img": "./images/product_images/tra_cherry_macchiato.jpg",
+        "name": "trà cherry macchiato",
+        "price": "65000",
+        "type": "trà",
+        "countTotal":0,
+        "issale":0
+    },
+]
+
+localStorage.setItem("listSanPhamLocalStory",JSON.stringify(listSanPham));
+let 
