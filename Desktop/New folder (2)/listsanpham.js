ee81f5a90197d299d9bc173d6b596c96fa27605f
Nguyen Anh Tuan <daobanhat991999@gmail.com>
2019-11-18 19:55:27 +0700

    dsfsd

--- Desktop/New folder (2)/listsanpham.js
+++ Desktop/New folder (2)/listsanpham.js
@@ -170,4 +170,43 @@ let listSanPham = [
 ]
 
 localStorage.setItem("listSanPhamLocalStory",JSON.stringify(listSanPham));
-let 
+
+//ham hien thi san pham
+let thong_tin_nguoi_dung=[]
+//ham hien thi san pham
+function displaySanPham(array,groupName,id) {
+    var group = [];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+    if (element.type === groupName){
+        group.push( element);
+    }
+    }
+console.log(group);
+    var s = "";
+    let itemcount = Math.round(group.length/3);
+    for(let i=0; i<itemcount; i++){
+        s += '<div class="row">'
+        for(let j=i*3; j < i*3+3; j++)
+            if(j < group.length){
+            const element1 = group[j];
+        console.log(group[j]);
+        
+            s += `<div class="col-sm-4" style="padding: 10px">
+                    <div class="card" style="rem">
+                        <img class="card-img-top" id="img_${j}" src="${element1.img}" alt="${element1.name}" style="width:100%; height:15rem">
+                        <div class="card-body">
+                            <h4  class="card-title" id="name_${j}">${element1.name}</h4>
+                            <p class="cost" price_${j}">${element1.price} Đ</p>
+                            <button type="button" class="btn btn-primary"  data-itemId="${element1.name}" id="${j}" onclick= "amountHandler(event)">mua ngay</button>
+                          
+                            
+                        </div>
+                    </div>
+                </div>`
+        }
+        s += "</div>"
+    } 
+    document.getElementById(id).innerHTML = s;
+}
+displaySanPham(listSanPham,"espresso",'Link_espresso');
